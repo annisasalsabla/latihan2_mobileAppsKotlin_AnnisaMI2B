9c2463f92b4149bfd18c33f14e90d94e80d403c0
Annisa Salsabila <162123987+annisasalsabla@users.noreply.github.com>
2024-08-27 15:00:41 +0700

    start

--- app/src/main/java/com/annisa/latihan2mobileappskotlin/MainActivity.kt
+++ app/src/main/java/com/annisa/latihan2mobileappskotlin/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.annisa.latihan2mobileappskotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val usernameEditText = findViewById<EditText>(R.id.usernameEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this,"Username atau password salah", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
